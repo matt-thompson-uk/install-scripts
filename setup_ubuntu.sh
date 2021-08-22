c692363f6a721d1e35862d55eca7ff34bd233955
matt <matt.thompson@nomail.com>
2021-08-22 08:15:10 +0100

    added stuff

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -9,6 +9,12 @@
 # install dialog if we don't already have it
 if ! command -v dialog >/dev/null; then
     echo "dialog is required - installing ...."
-    sudo apt install -y dialog
-
+    sudo apt install dialog -y
 fi
+
+if dialog --clear --yesno  "This will setup an Ubuntu based system. Continue?" 0 0; then
+  if dialog --clear --yesno "Step 1 - Do you want to update the system?" 0 0; then
+    clear
+    sudo apt update
+    sudo apt upgrade -y
+  fi
